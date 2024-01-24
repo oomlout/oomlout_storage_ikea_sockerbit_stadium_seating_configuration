d5a5c167fe40e3ae75fc9faa5d96c4e67d2ff3f7
Oom Lout <aaron@oomlout.com>
2024-01-24 09:57:30 +0000

    initial

--- scad.py
+++ scad.py
@@ -2,6 +2,8 @@ import copy
 import opsc
 import oobb
 import oobb_base
+import math
+import yaml
 
 def main(**kwargs):
     make_scad(**kwargs)
@@ -11,16 +13,16 @@ def make_scad(**kwargs):
 
     # save_type variables
     if True:
-        #filter = ""
-        filter = "test"
+        filter = ""
+        #filter = []
 
-        #kwargs["save_type"] = "none"
+        kwargs["save_type"] = "none"
         kwargs["save_type"] = "all"
         
         kwargs["overwrite"] = True
         
-        kwargs["modes"] = ["3dpr", "laser", "true"]
-        #kwargs["modes"] = ["3dpr"]
+        #kwargs["modes"] = ["3dpr", "laser", "true"]        
+        kwargs["modes"] = ["3dpr"]
         #kwargs["modes"] = ["laser"]
 
     # default variables
@@ -28,83 +30,262 @@ def make_scad(**kwargs):
         kwargs["size"] = "oobb"
         kwargs["width"] = 12
         kwargs["height"] = 12
-        kwargs["thickness"] = 6
+        thickness = 9
+        kwargs["thickness"] = thickness
 
     # project_variables
     if True:
-        pass
+        #cycloidal ones
+        kwargs["sockerbit_lip_width"] = 15
+        kwargs["sockerbit_lip_depth"] = 15
+        kwargs["sokerbit_plastic_thickness"] = 2
+        kwargs["bin_spacing"] = 5
+        kwargs["thickness_default"] = 3
+        
+
     
     # declare parts
     if True:
-
         part_default = {} 
-        part_default["project_name"] = "test" ####### neeeds setting
+        part_default["project_name"] = "oomlout_storage_ikea_sockerbit_stadium_seating_configuration"
         part_default["full_shift"] = [0, 0, 0]
         part_default["full_rotations"] = [0, 0, 0]
         
         part = copy.deepcopy(part_default)
         p3 = copy.deepcopy(kwargs)
-        p3["thickness"] = 6
+        p3["thickness"] = thickness
         part["kwargs"] = p3
-        part["name"] = "test"
+        part["name"] = "clamp_top"        
+        parts.append(part)
+        
+        part = copy.deepcopy(part_default)
+        p3 = copy.deepcopy(kwargs)
+        p3["thickness"] = thickness
+        part["kwargs"] = p3
+        part["name"] = "clamp_bottom"        
         parts.append(part)
 
         
     #make the parts
     if True:
         for part in parts:
-            name = part.get("name", "default")
-            if filter in name:
+            name = part.get("name", "default")            
+            if not isinstance(filter, list):
+                filter = [filter]
+            generate = False
+            for f in filter:
+                if f in name:
+                    generate = True
+            if generate:
                 print(f"making {part['name']}")
                 make_scad_generic(part)            
                 print(f"done {part['name']}")
             else:
                 print(f"skipping {part['name']}")
 
-def get_test(thing, **kwargs):
+    #dump parts to scad_output/working.yaml
+    if True:
+        yaml_file = "scad_output/working.yaml"
+        with open(yaml_file, 'w') as file:
+            yaml.dump(parts, file)
+    
 
-    depth = kwargs.get("thickness", 4)
+
+def get_clamp_bottom(thing, **kwargs):
+    center_offset = kwargs.get("center_offset", True)
+
+    thickness = kwargs.get("thickness", 4)
     prepare_print = kwargs.get("prepare_print", True)
 
     pos = kwargs.get("pos", [0, 0, 0])
     #pos = copy.deepcopy(pos)
     #pos[2] += -20
 
-    #add _cylinder
+
+    #varaiables
+    sokerbit_lip_width  = kwargs.get("sockerbit_lip_width", 15)
+    sokerbit_lip_depth  = kwargs.get("sockerbit_lip_depth", 15)
+    sokerbit_plastic_thickness  = kwargs.get("sokerbit_plastic_thickness", 2)
+    bin_spacing  = kwargs.get("bin_spacing", 5)
+    thickness_default  = kwargs.get("thickness_default", 3)
+
+
+    #add _cube_middle
     p3 = copy.deepcopy(kwargs)
     p3["type"] = "p"
-    p3["shape"] = f"oobb_cylinder"
-    p3["radius"] = 30
-    p3["depth"] = depth
-    #p3["m"] = "#"
-    pos1 = copy.deepcopy(pos)         
+    p3["shape"] = f"oobb_cube"
+    width = bin_spacing
+    height = sokerbit_lip_depth
+    depth = thickness
+    p3["size"] = [width, height, depth]   
+    pos1 = copy.deepcopy(pos)     
+    pos1[1] += -sokerbit_lip_depth/2
+    pos1[2] += -depth/2
     p3["pos"] = pos1
     oobb_base.append_full(thing,**p3)
 
-    
+    #add cube_cross
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "p"
+    p3["shape"] = f"oobb_cube"
+    width = bin_spacing + 2 * sokerbit_lip_width
+    height = thickness_default
+    depth = thickness
+    p3["size"] = [width, height, depth]   
+    pos1 = copy.deepcopy(pos)     
+    pos1[1] += -sokerbit_lip_depth - thickness_default/2
+    pos1[2] += -depth/2
+    p3["pos"] = pos1
+    oobb_base.append_full(thing,**p3)
+
+    # add joining screw
+    get_joining_screw(thing, **kwargs)
+
 
     if prepare_print:
         #put into a rotation object
-        components_second = copy.deepcopy(thing["components"])
-        return_value_2 = {}
-        return_value_2["type"]  = "rotation"
-        return_value_2["typetype"]  = "p"
-        pos1 = copy.deepcopy(pos)
-        pos1[0] += 50
-        return_value_2["pos"] = pos1
-        return_value_2["rot"] = [180,0,0]
-        return_value_2["objects"] = components_second
-        
-        thing["components"].append(return_value_2)
+        if False:
+            components_second = copy.deepcopy(thing["components"])
+            return_value_2 = {}
+            return_value_2["type"]  = "rotation"
+            return_value_2["typetype"]  = "p"
+            pos1 = copy.deepcopy(pos)
+            pos1[0] += 50
+            return_value_2["pos"] = pos1
+            return_value_2["rot"] = [180,0,0]
+            return_value_2["objects"] = components_second
+            
+            thing["components"].append(return_value_2)
 
+        
+            #add slice # top
+            p3 = copy.deepcopy(kwargs)
+            p3["type"] = "n"
+            p3["shape"] = f"oobb_slice"
+            #p3["m"] = "#"
+            oobb_base.append_full(thing,**p3)
     
-        #add slice # top
-        p3 = copy.deepcopy(kwargs)
-        p3["type"] = "n"
-        p3["shape"] = f"oobb_slice"
-        #p3["m"] = "#"
-        oobb_base.append_full(thing,**p3)
+
+def get_clamp_top(thing, **kwargs):
+    center_offset = kwargs.get("center_offset", True)
+
+    thickness = kwargs.get("thickness", 4)
+    prepare_print = kwargs.get("prepare_print", True)
+
+    pos = kwargs.get("pos", [0, 0, 0])
+    #pos = copy.deepcopy(pos)
+    #pos[2] += -20
+
+
+    #varaiables
+    sokerbit_lip_width  = kwargs.get("sockerbit_lip_width", 15)
+    sokerbit_lip_depth  = kwargs.get("sockerbit_lip_depth", 15)
+    sokerbit_plastic_thickness  = kwargs.get("sokerbit_plastic_thickness", 2)
+    bin_spacing  = kwargs.get("bin_spacing", 5)
+    thickness_default  = kwargs.get("thickness_default", 3)
+
+
+    #add _cube_middle
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "p"
+    p3["shape"] = f"oobb_cube"
+    width = bin_spacing + 2 * sokerbit_lip_width + thickness_default * 2
+    height = thickness_default
+    depth = thickness
+    p3["size"] = [width, height, depth]   
+    pos1 = copy.deepcopy(pos)     
+    pos1[1] += thickness_default/2
+    pos1[2] += -depth/2
+    p3["pos"] = pos1
+    oobb_base.append_full(thing,**p3)
+
+    #add down x 2
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "p"
+    p3["shape"] = f"oobb_cube"
+    width = thickness_default
+    height = sokerbit_lip_depth
+    depth = thickness
+    p3["size"] = [width, height, depth]   
+    poss = []
+    pos1 = copy.deepcopy(pos)     
+    pos1[2] += -depth/2
+    pos11 = copy.deepcopy(pos1)
+    pos11[0] += -bin_spacing / 2 - sokerbit_lip_width - thickness_default / 2
+    pos11[1] += -sokerbit_lip_depth / 2
+    pos12 = copy.deepcopy(pos1)
+    pos12[0] += bin_spacing / 2 + sokerbit_lip_width + thickness_default / 2
+    pos12[1] += -sokerbit_lip_depth / 2
+    poss.append(pos11)
+    poss.append(pos12)
+    p3["pos"] = poss
+    oobb_base.append_full(thing,**p3)
+
+    # add joining screw
+    get_joining_screw(thing, **kwargs)
+
+
+    if prepare_print:
+        #put into a rotation object
+        if False:
+            components_second = copy.deepcopy(thing["components"])
+            return_value_2 = {}
+            return_value_2["type"]  = "rotation"
+            return_value_2["typetype"]  = "p"
+            pos1 = copy.deepcopy(pos)
+            pos1[0] += 50
+            return_value_2["pos"] = pos1
+            return_value_2["rot"] = [180,0,0]
+            return_value_2["objects"] = components_second
+            
+            thing["components"].append(return_value_2)
+
+        
+            #add slice # top
+            p3 = copy.deepcopy(kwargs)
+            p3["type"] = "n"
+            p3["shape"] = f"oobb_slice"
+            #p3["m"] = "#"
+            oobb_base.append_full(thing,**p3)
     
+
+
+
+
+
+def get_joining_screw(thing, **kwargs):
+    #varaiables
+    pos = kwargs.get("pos", [0, 0, 0])
+    sokerbit_lip_width  = kwargs.get("sockerbit_lip_width", 15)
+    sokerbit_lip_depth  = kwargs.get("sockerbit_lip_depth", 15)
+    sokerbit_plastic_thickness  = kwargs.get("sokerbit_plastic_thickness", 2)
+    bin_spacing  = kwargs.get("bin_spacing", 5)
+    thickness_default  = kwargs.get("thickness_default", 3)
+    #add joining_screw
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "n"
+    p3["shape"] = f"oobb_screw_countersunk"
+    p3["radius_name"] = "m3"
+    p3["depth"] = thickness_default * 2 + sokerbit_lip_depth
+    #p3["m"] = "#"
+    pos1 = copy.deepcopy(pos)     
+    pos1[1] += thickness_default
+    p3["pos"] = pos1
+    p3["nut"] = True
+    p3["rot"] = [-90,0,0]
+    #p3["m"] = "#"
+    oobb_base.append_full(thing,**p3)
+
+
+
+
+
+
+
+
+
+
+
 ###### utilities
 
 
